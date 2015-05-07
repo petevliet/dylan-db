40d15e7fd3b8db4c4413d932341452f0c3bc4b71
Peter Vliet <petervliet@gmail.com>
2015-05-06 18:51:25 -0600

    musicgrabber model can access dylan albums

--- app/models/music_grabber.rb
+++ app/models/music_grabber.rb
@@ -1,7 +1,19 @@
 class MusicGrabber
 
   def initialize
-    
+    @conn = Faraday.new(:url => 'https://partner.api.beatsmusic.com/') do |faraday|
+      faraday.request  :url_encoded             # form-encode POST params
+      faraday.response :logger                  # log requests to STDOUT
+      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    end
+  end
+
+  def get_dylan
+    response = @conn.get do |req|
+      req.url "v1/api/artists/ar3782/albums?"
+      req.params['client_id'] = ENV['BEATS_API_KEY']
+      # req.headers['Content-Type'] = 'application/json'
+    end
   end
 
 end
