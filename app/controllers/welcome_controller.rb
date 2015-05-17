8a631c1e6573bf8727244a87aecb629a0bf42f27
Peter Vliet <petervliet@gmail.com>
2015-05-17 17:10:01 -0600

    all dylans album titles show on welcome#index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,17 @@
 class WelcomeController < ApplicationController
 
   def index
+    @titles = []
+    @dylan_lookup = MusicGrabber.new
+    @dylan_albums = @dylan_lookup.get_dylan
+    albums = @dylan_albums["data"]
+    albums.each do |album|
+      @titles << album["title"]
+    end
+  end
 
+  def show
+    
   end
 
 end
--- app/models/music_grabber.rb
+++ app/models/music_grabber.rb
@@ -6,11 +6,21 @@ class MusicGrabber
       faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
+    @albums = []
   end
 
   def get_dylan
     response = @conn.get do |req|
-      req.url "v1/api/artists/ar3782/albums?"
+      req.url "v1/api/artists/ar3782/albums?limit=80"
+      req.params['client_id'] = ENV['BEATS_API_KEY']
+    end
+
+    his_albums = JSON.parse(response.body)
+  end
+
+  def get_album(album_name)
+    response = @conn.get do |req|
+      req.url "v1/api/albums/#{album_name}?"
       req.params['client_id'] = ENV['BEATS_API_KEY']
     end
   end
