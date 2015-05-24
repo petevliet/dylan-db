9a0b9798d55718be5da6aba390db6427eeec42a4
Peter Vliet <petervliet@gmail.com>
2015-05-24 12:23:35 -0600

    add lyrics to tracks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519002605) do
+ActiveRecord::Schema.define(version: 20150524182126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150519002605) do
     t.string  "title"
     t.string  "track_id"
     t.integer "album_id"
+    t.text    "lyrics"
   end
 
 end
