ab104279e8960fa4e5c988cb73a7a93a088dec63
Peter Vliet <petervliet@gmail.com>
2015-05-26 16:37:02 -0600

    add user model and user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150526202543) do
+ActiveRecord::Schema.define(version: 20150526223430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 20150526202543) do
     t.integer "album_id"
     t.text    "lyrics"
     t.text    "verses",   default: [], array: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
