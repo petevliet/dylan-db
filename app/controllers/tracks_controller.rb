56c1fb41a7f1a9e6f4e3d8fdf7f710a0489406df
Peter Vliet <petervliet@gmail.com>
2015-05-26 11:06:58 -0600

    song lyrics retrieved and displayed on tracks#show

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,7 +2,15 @@ class TracksController < ApplicationController
 
   def show
     @track = Track.find(params[:id])
-    
+    songs = RapGenius.search_by_title(@track.title)
+    dylan_version = []
+    songs.each do |s|
+      if s.artist.name == "Bob Dylan"
+        dylan_version << s
+      end
+    end
+    lyrics_raw = dylan_version[0].response["lyrics"]["plain"].html_safe
+    @lyrics = lyrics_raw.split("\n").join("</p><p>").html_safe
   end
 
 end
