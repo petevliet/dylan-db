70e90ed921f3e22d8c647f07720d357a3f649141
Peter Vliet <petervliet@gmail.com>
2015-05-26 14:34:44 -0600

    take away title tags like remastered in api calls

--- app/models/lyrics_grabber.rb
+++ app/models/lyrics_grabber.rb
@@ -5,7 +5,14 @@ class LyricsGrabber
   end
 
   def get_lyrics(track)
-    songs = RapGenius.search_by_title(track.title)
+    # if title has tags that inhibit lyric api calls, remove them
+    if track.title.include?("Remastered" || "Live" || "Single")
+      song_title = track.title.slice(/\A[^(]+/).strip
+    else
+      song_title = track.title
+    end
+    songs = RapGenius.search_by_title(song_title)
+    # RapGenius query returns songs from different artists, make sure only Dylan's versions show
     dylan_version = []
     songs.each do |s|
       if s.artist.name == "Bob Dylan"
@@ -13,8 +20,10 @@ class LyricsGrabber
       end
     end
     lyrics_raw = dylan_version[0].response["lyrics"]["plain"]
+    # replace newspace markup with close->open p tags so lyrics can be displayed properly
     @lyrics = lyrics_raw.split("\n").join("</p><p>")
     track.lyrics = @lyrics
+    binding.pry
     track.save
   end
 
