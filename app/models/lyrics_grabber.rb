9c3844c7d2f5327f19c1d4f27f0fb8d00b854f28
Peter Vliet <petervliet@gmail.com>
2015-05-27 15:54:49 -0600

    i realize todays efforts have been unnecessary

--- app/models/lyrics_grabber.rb
+++ app/models/lyrics_grabber.rb
@@ -28,18 +28,21 @@ class LyricsGrabber
     verse_split = lyrics_html.split("[Verse ")
       verse_split.delete_if {|verse_element| verse_element.empty?}
     verse_split.each do |verse|
-      verse.prepend("[Verse ")
+      if verse.match(/\A[0-9]/) != nil
+        verse.prepend("[Verse ")
+      else
+        verse.prepend("[")
+      end
       track.verses << verse
     end
     # if lyrics include a tagged chorus, the first chorus' contents must be preserved while subsequent chorus tags removed from lyrics so lyrics can be properly commented upon.
-    if track.lyrics.include?("Chorus]")
-      first_chorus_index = track.verses.index{|verse| verse.include?("Chorus")}
-      chorus = track.verses.slice!(first_chorus_index)
-      track.verses.each {|verse| verse.slice!("[Chorus]")}
-      track.verses.insert(first_chorus_index, chorus)
-      track.verses[first_chorus_index].sub("[Verse Chorus]", "[Chorus]")
-      binding.pry
-    end
+    # if track.lyrics.include?("Chorus]")
+    #   first_chorus_index = track.verses.index{|verse| verse.include?("Chorus")}
+    #   chorus = track.verses.slice!(first_chorus_index)
+    #   track.verses.each {|verse| verse.slice!("[Chorus]")}
+    #   track.verses.insert(first_chorus_index, chorus)
+    # end
+    track.lyrics.sub("[Verse Chorus]", "[Chorus]")
     track.save
   end
 
