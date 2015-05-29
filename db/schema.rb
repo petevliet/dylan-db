cc3e652d4bc027f9629a3014266aae5c9602e067
Peter Vliet <petervliet@gmail.com>
2015-05-29 12:56:27 -0600

    add createdat to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529185038) do
+ActiveRecord::Schema.define(version: 20150529185558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,8 +41,9 @@ ActiveRecord::Schema.define(version: 20150529185038) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "nickname"
+    t.string   "name"
+    t.string   "nickname"
+    t.datetime "created_at"
   end
 
 end
