caddd9c3ed856f5627869dd326875b29b3ed1a1c
Peter Vliet <petervliet@gmail.com>
2015-05-29 13:05:10 -0600

    create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,18 @@ class SessionsController < ApplicationController
     user = User.find_or_create_by(twitter_id: uid)
 
     user.update_attributes(name: env['omniauth.auth']['info']['name'],
-                        email: env['omniauth.auth']['info']['email'],
                         nickname: env['omniauth.auth']['info']['nickname'].downcase,
                         user_token: env['omniauth.auth']['credentials']['token'],
                         user_token_secret: env['omniauth.auth']['credentials']['secret'])
 
     redirect_to root_path, notice: "You're logged in!"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    session[:user_token] = nil
+    session[:user_secret] = nil
+    redirect_to root_path, notice: "Logged out."
   end
 
 end
