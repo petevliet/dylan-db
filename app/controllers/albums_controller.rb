db10e2bfe81215a41c1af2cf4be26633bc382d55
Peter Vliet <petervliet@gmail.com>
2015-06-04 14:53:13 -0600

    add live album#show background

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -18,7 +18,6 @@ class AlbumsController < ApplicationController
       @dylan_lookup.get_album(@album.album_num)
     end
     @album_tracks = @album.tracks.order(:track_id)
-    # if @album.title.includes
     @year = @album.release_date.strftime("%Y").to_i
   end
 
