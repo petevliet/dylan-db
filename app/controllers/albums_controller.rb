4c1d82dde2dd45bb784c2904a9977709f8760221
Peter Vliet <petervliet@gmail.com>
2015-06-05 14:16:27 -0600

    add album comment count on albums#show

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -24,6 +24,7 @@ class AlbumsController < ApplicationController
     end
     @album_tracks = @album.tracks.order(:track_id)
     @year = @album.release_date.strftime("%Y").to_i
+    @album_comments = @album.tracks.inject(0) {|sum, track| sum + track.comments.length}
     if @album.album_review != "n/a"
       @review = @album.album_review.split("~")
       @author_and_source = @review[1].split(",")
