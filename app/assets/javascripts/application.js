c310d3e0d34678caee70bedea165a28e2a79900e
Peter Vliet <petervliet@gmail.com>
2015-06-08 21:25:09 -0600

    modify album#index slideDown times

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -79,7 +79,7 @@ $(document).ready(function(){
       });
       albumBoxes.hide();
       $(checkedTypesInYears).each(function(i, element){
-        $(element).show();
+        $(element).slideDown(400);
       });
     }
     // iterate through each selected album type checkbox, album, and album type and show if values match
@@ -89,7 +89,7 @@ $(document).ready(function(){
           $($(el).data('types')).each(function(i, albumType){
             if(albumType === $(type).val()){
               // if album type matches selected checkbox type, make album visible
-              $(el).show();
+              $(el).slideDown(400);
             }
           });
         });
@@ -100,7 +100,7 @@ $(document).ready(function(){
         $(albumBoxes).each(function(i, el){
           // if album year falls within checkedBox range, make album visible
           if(parseInt($(el).data('year-released')) >= parseInt($(years).val()) && parseInt($(el).data('year-released')) < parseInt($(years).val()) + 10){
-            $(el).show();
+            $(el).slideDown(400);
           }
         });
       });
@@ -244,7 +244,7 @@ $(document).ready(function(){
   function go_to_page(page_num){
     var start_from = (page_num - 1) * show_per_page;
     var end_on = start_from + show_per_page;
-    $(".well-albums-index").children().hide().slice(start_from, end_on).slideDown(800);
+    $(".well-albums-index").children().hide().slice(start_from, end_on).slideDown(400);
     $('body, .well-albums-container').animate({
       scrollTop: $("#albums").offset().top
     }, 1600);
