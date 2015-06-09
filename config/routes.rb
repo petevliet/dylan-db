0afe05270cff0fd88a0c5a2a2b591ec71d774940
Peter Vliet <petervliet@gmail.com>
2015-06-09 07:03:15 -0600

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get '/albums/:album_id/tracks/:track_id/comments/new' => 'tracks#new_comment', :as => :new_comment
-  resources :albums do
-    resources :tracks do
+  resources :albums, only: [:index, :show] do
+    resources :tracks, only: [:index, :show] do
       resources :comments
     end
   end
