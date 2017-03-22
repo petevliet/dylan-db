af300c5853bc570b59903654105efccabf347460
Peter Vliet <plvliet@us.ibm.com>
2017-03-22 13:24:15 -0700

    remove js comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,28 +16,23 @@
 //= require_tree .
 $(document).ready(function(){
 
-  // number albums want to display
   var show_per_page = 12;
-  // number of total albums set to display
   var number_of_albums = $(".well-albums-index").children().size();
-  // number of pages there will be, plus one because #current_page starts on 1
   var number_of_pages = Math.ceil(number_of_albums/show_per_page + 1);
-  // set to first page- value will be hidden in html
+
   $("#current_page").val(1);
   $("#show_per_page").val(show_per_page);
-  // page number 1 is default active page
   $("#1").addClass("active_page");
-  // initially, make all albums hidden
+
   $(".well-albums-index").children().css("display", "none");
-  // then, show only the first albums in the per page range
   $(".well-albums-index").children().slice(0, show_per_page).css("display", "inline-block");
-  // for all page# pagination icons, run go_to_page when clicked on
+
   for(var x = 1; x < number_of_pages; x++){
     $("#" + x).on("click", function(e){
       go_to_page(parseInt(this.id));
     });
   }
-  // append album count above pagination icons
+
   $("#albums_count").append("<center>Showing albums 1 - 12 of " + number_of_albums + "</center>");
 
   $("#previous").on("click", function(){
@@ -47,13 +42,12 @@ $(document).ready(function(){
   $("#next").on("click", function(){
     next();
   });
-  // narrow search according to checkboxes on albums#index when 'narrow' button clicked
+
   $("#narrow-search").on("click", function(){
-    // place year and type selected checkboxes into separate arrays
     var checkedYears = $("#years").find(":checkbox").filter(":checked");
     var checkedTypes = $("#types").find(":checkbox").filter(":checked");
     var albumBoxes = $(".well-albums-index").children()
-    // reload page if no checkboxes selected and narrowsearch is clicked
+
     if (checkedYears.length === 0 && checkedTypes.length === 0){
       location.reload();
     };
@@ -63,7 +57,6 @@ $(document).ready(function(){
         $(albumBoxes).each(function(i, el){
           $($(el).data('types')).each(function(i, albumType){
             if(albumType === $(type).val()){
-              // if album type matches selected checkbox type, make album visible
               $(el).show();
             }
           });
@@ -82,32 +75,26 @@ $(document).ready(function(){
         $(element).slideDown(400);
       });
     }
-    // iterate through each selected album type checkbox, album, and album type and show if values match
     else if (checkedTypes.length > 0 && checkedYears.length === 0){
       $(checkedTypes).each(function(i, type){
         $(albumBoxes).each(function(i, el){
           $($(el).data('types')).each(function(i, albumType){
             if(albumType === $(type).val()){
-              // if album type matches selected checkbox type, make album visible
               $(el).slideDown(400);
             }
           });
         });
       });
-    // for each checked box, iterate through each albumBox
   } else if (checkedYears.length > 0 && checkedTypes.length === 0){
       $(checkedYears).each(function(i, years){
         $(albumBoxes).each(function(i, el){
-          // if album year falls within checkedBox range, make album visible
           if(parseInt($(el).data('year-released')) >= parseInt($(years).val()) && parseInt($(el).data('year-released')) < parseInt($(years).val()) + 10){
             $(el).slideDown(400);
           }
         });
       });
     }
-    // assign variable to num of visible albums
     var visibleAlbums = $("div.panel-body:visible").length;
-    // results will be shown in one well container
     $(".pagination").remove();
     $("#albums_count").replaceWith("<div id='albums_count'><center>Showing albums 1 - " + visibleAlbums + " of " + visibleAlbums + "</center></div>");
     $('html, body').animate({
@@ -115,12 +102,10 @@ $(document).ready(function(){
     }, 1000);
   });
 
-  // album box images become active/opaque when hovered over
   $(".album-box").hover(
     function(){
       $(this).removeClass("panel-default");
       $(this).addClass("panel-info album-box-active");
-      // so buttons do not show shadow effect
       $($(this).find(".btn-xs")[0]).removeClass("btn-link");
       $($(this).find(".btn-xs")[0]).addClass("btn-info album-box-active-button");
       $($(this).find("img")).fadeTo(200, 1.0);
@@ -137,7 +122,6 @@ $(document).ready(function(){
     $('[data-toggle="popover"]').popover()
   });
 
-  // individual tracks on album#show become blue when hovered over
   $(".album-track").hover(
     function(){
       $(this).addClass("active");
@@ -152,11 +136,9 @@ $(document).ready(function(){
     }
   );
 
-  // on tracks#show, individual verses become active and highlighted when hovered over
   $(".lyric-section").hover(
     function(){
     if (!$(this).hasClass("active")){
-      // using jquery_color plugin
       $(this).animate({
         backgroundColor: "#E2F9FB",
         fontSize: "1.2em"
@@ -174,9 +156,7 @@ $(document).ready(function(){
     }
   );
 
-  // when verses are clicked on, they will retain a new color beyond hover
   $(".lyric-section").on("click", function(){
-    // if another verse has already been clicked, only newest clicked verse will be active
     var oldClickedVerse = $(".lyrics").find(".active");
     if (oldClickedVerse.length > 0){
       $(oldClickedVerse[0]).removeClass("active");
@@ -191,7 +171,6 @@ $(document).ready(function(){
       backgroundColor: "#EEEEEE",
       fontSize: "1.2em"
     }, 100);
-    // if clicked verse data equals comment well data, show comment well
     $(".track-show .well").each(function(i, el){
       if ($(clickedVerse).data("verse-comments") == $(el).data("verse-number")){
         $(el).slideDown(200);
@@ -201,7 +180,6 @@ $(document).ready(function(){
     });
   });
 
-  // when close button is clicked on comments well, close well and deselect lyrics section
   $(".close-comments").on("click", function(){
     $(".track-show .well").slideUp(100);
     var clickedVerse = $(".lyrics").find(".active");
@@ -212,26 +190,20 @@ $(document).ready(function(){
     }, 100);
   });
 
-  // when add new comment modal is opened
   $('#newCommentModal').on('show.bs.modal', function (event) {
-    // so previously loaded verses dont get retained in modals
     $(".verse-section").empty();
-    // place verse text in top portion on modal
     var activeVerseElement = $("#lyrics-scroll").find(".active")[0];
     var verseLyrics = $($(activeVerseElement).find("#verse")[0]).html();
     $(".verse-section").append(verseLyrics);
-    // retrieve verse data so it can be placed into hidden rails form element
     var button = $(event.relatedTarget);
     var verseNum = $(button[0]).data('verse-number-comment');
     var verseNumEntry = $(this).find("#verseNumEntry")[0];
     $(verseNumEntry).val(verseNum);
   });
 
-  // on tracks#index, individual song boxes become active and highlighted when hovered over
   $(".songs").hover(
     function(){
       $(this).find("a")[0];
-      // using jquery_color plugin
       $(this).animate({
         backgroundColor: "#E2F9FB"
       }, 50);
@@ -242,7 +214,6 @@ $(document).ready(function(){
     }
   );
 
-  // letters on tracks#index except for x and z which dont have songs, become big and white when hovered over
   $(".letter-click").hover(
     function(){
       if (!(this.id === "X" || this.id === "Z")){
@@ -261,7 +232,6 @@ $(document).ready(function(){
     }
   );
 
-  // when a letter is clicked on tracks#index, hide current letter's box and display selected letter's box, and fade in current letter
   $(".letter-click").on("click", function(){
     if (!(this.id === "X" || this.id === "Z")){
       var previousSelected = $(".songbox").filter(":visible")[0]
@@ -315,6 +285,5 @@ $(document).ready(function(){
       $("#albums_count").replaceWith("<div id='albums_count'><center>Showing albums " + (start_from + 1) + " - " + (end_on) + " of " + number_of_albums + "</center></div>");
       $("#next").removeClass("disabled");
     }
-
   }
 });
