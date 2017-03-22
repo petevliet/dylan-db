7bce0dc1208eac0cf4d92bb7c8587746d5574747
Peter Vliet <plvliet@us.ibm.com>
2017-03-22 10:13:35 -0700

    no longer use beats API

--- app/models/music_grabber.rb
+++ app/models/music_grabber.rb
@@ -1,65 +1,12 @@
 class MusicGrabber
 
   def initialize
-    @conn = Faraday.new(:url => 'https://partner.api.beatsmusic.com/') do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
-    end
     @spotify_collection = Faraday.new(:url => 'https://api.spotify.com/') do |faraday|
       faraday.request  :url_encoded             # form-encode POST params
       faraday.response :logger                  # log requests to STDOUT
       faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
     end
     @albums = []
-  end
-
-  def get_dylan
-    response = @conn.get do |req|
-      req.url "v1/api/artists/ar3782/albums?limit=80"
-      req.params['client_id'] = ENV['BEATS_API_KEY']
-    end
-
-    his_albums = JSON.parse(response.body)
-    his_albums["data"].each do |new_album|
-      new_dylan_album = Album.new
-      new_dylan_album.title = new_album["title"]
-      new_dylan_album.release_date = new_album["release_date"]
-      new_dylan_album.album_num = new_album["id"]
-      new_dylan_album.save
-    end
-
-  end
-
-  def get_album(album_id)
-    response = @conn.get do |req|
-      req.url "v1/api/albums/#{album_id}?"
-      req.params['client_id'] = ENV['BEATS_API_KEY']
-    end
-
-    dylan_album = JSON.parse(response.body)
-    dylan_album["data"]["refs"]["tracks"].each do |new_album_info|
-      track_info = Track.new
-      track_info.title = new_album_info["display"]
-      track_info.track_id = new_album_info["id"]
-      track_info.album_id = Album.find_by(album_num: dylan_album["data"]["id"]).id
-      track_info.save
-    end
-  end
-
-  def get_review(album_id)
-    album = Album.find_by(album_num: album_id)
-    response = @conn.get do |req|
-      req.url "v1/api/albums/#{album_id}/review?"
-      req.params['client_id'] = ENV['BEATS_API_KEY']
-    end
-    review_json = JSON.parse(response.body)
-    if review_json["code"] == "ResourceNotFound"
-      album.album_review = "n/a"
-    else
-      album.album_review = review_json["data"]["content"]
-    end
-    album.save
   end
 
   def get_art_from_spotify
@@ -84,16 +31,12 @@ class MusicGrabber
           db_album.image_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Bob_Dylan_Barcelona.jpg/360px-Bob_Dylan_Barcelona.jpg"
           db_album.large_image_url = "https://c1.staticflickr.com/7/6155/6158417919_f8059f091c_b.jpg"
           db_album.save
+          break
         elsif spotify_album["name"].include? db_album.title || spotify_album["name"].downcase == db_album.title.downcase
           db_album.image_url = spotify_album["images"][1]["url"]
           db_album.large_image_url = spotify_album["images"][0]["url"]
           db_album.save
           break
-        # elsif
-        #   db_album.image_url = spotify_album["images"][1]["url"]
-        #   db_album.large_image_url = spotify_album["images"][0]["url"]
-        #   db_album.save
-        #   break
         end
       end
       if spotify_album["name"] == "Dylan & The Dead"
