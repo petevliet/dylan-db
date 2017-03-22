7210e334d3e6d5925619da20182dda5f5aae2fe9
Peter Vliet <plvliet@us.ibm.com>
2017-03-22 13:28:33 -0700

    remove controller comments

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -3,21 +3,20 @@ class AlbumsController < ApplicationController
   def index
     @albums = Album.all.order(:release_date)
     @show_pages = @albums.count / 12 + 1
-
   end
 
   def show
     @album = Album.find(params[:id])
-    # populate album tracks if there aren't any yet
+
     if @album.tracks.length == 0
       @dylan_lookup = MusicGrabber.new
       @dylan_lookup.get_album(@album.album_num)
     end
     @album_tracks = @album.tracks.order(:track_id)
     @year = @album.release_date.strftime("%Y").to_i
-    # calculate number of total comments for all album's tracks
+
     @album_comments = @album.tracks.inject(0) {|sum, track| sum + track.comments.length}
-    # prepare album review for display, if there is one
+
     if @album.album_review != "n/a"
       @review = @album.album_review.split("~")
       @author_and_source = @review[1].split(",")
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -10,30 +10,26 @@ class TracksController < ApplicationController
       end
     end
     # select tracks from biograph that are unreleased
-    biograph_tracks = Track.all.select {|track| track.title == "Percy's Song" || track.title == "Mixed-Up Confusion" || track.title == "Groom's Still Waiting At The Altar" || track.title == "Jet Pilot" || track.title == "Lay Down Your Weary Tune" || track.title == "Quinn The Eskimo (The Mighty Quinn)" || track.title == "Abandoned Love" || track.title == "Can You Please Crawl Out Your Window?" || track.title == "Positively 4th Street" || track.title == "Caribbean Wind" || track.title == "Up To Me" || track.title == "Baby, I'm In The Mood For You" || track.title == "I Wanna Be Your Lover"}
+    biograph_tracks = Track.all.select {|track| track.title == "Percy's Song" || track.title == "Mixed-Up Confusion" || track.title == "Groom's Still Waiting At The Altar" || track.title == "Jet Pilot" || track.title == "Lay Down Your Weary Tune" || track.title == "Quinn The Eskimo (The Mighty Quinn)" ||
+       track.title == "Abandoned Love" || track.title == "Can You Please Crawl Out Your Window?" || track.title == "Positively 4th Street" || track.title == "Caribbean Wind" || track.title == "Up To Me" || track.title == "Baby, I'm In The Mood For You" || track.title == "I Wanna Be Your Lover"}
     tracks << biograph_tracks
-    # all_tracks is collection of all tracks to be shown on tracks#index
+
     all_tracks = tracks.flatten
     @alphabet_array = []
-    # array of cap letters A-Z
     @letters_array = ('A'..'Z').to_a
-    # empty hash where each letter will be new key
     @letters_hash = Hash.new {|k, v| k[v] = []}
-    # for each letter, iterate through all_tracks collection and shovel track into hash letter key's value that title begins with
     @letters_array.each do |letter|
       all_tracks.each do |track|
           if track.title.start_with?(letter)
             @letters_hash[letter] << track
           end
         end
-        # alphabatize each key's values values by title
         @alphabet_array << @letters_hash[letter].sort_by{|k,v| k["title"]}
       end
   end
 
   def show
     @track = Track.find(params[:id])
-    # if track does not yet have lyrics, use lyricsgrabber to retrieve them
     if @track.lyrics == nil
       lyric_query = LyricsGrabber.new
       lyric_query.find_lyrics(@track)
