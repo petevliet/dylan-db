8055aa3719298837f5252819a6a8cfe0c0aede4b
Peter Vliet <plvliet@us.ibm.com>
2017-03-22 11:21:15 -0700

    debug one take two

--- app/models/music_grabber.rb
+++ app/models/music_grabber.rb
@@ -38,7 +38,9 @@ class MusicGrabber
         elsif spotify_album["name"] == db_album.title
           db_album.image_url = spotify_album["images"][1]["url"]
           db_album.large_image_url = spotify_album["images"][0]["url"]
-          if db_album.title = "Planet Waves" {|planet_waves| p db_album}
+          if db_album.title = "Planet Waves"
+            p db_album
+          end
           db_album.save
           break
         elsif spotify_album["name"].include? db_album.title
